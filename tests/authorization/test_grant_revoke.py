b747fe1545ca2d60a0f65d4a6239382767c1d9d8
aphadke <aphadke@cloudera.com>
2017-06-27 12:14:04 -0700

    IMPALA-5582: Store sentry privileges in lower case
    Privileges granted to role on a db/table whose name
    contains upper case characters can disappear after a few seconds.
    A privilege is inserted into the catalogObjectCache using a key
    that uses the db/table name. The key gets converted to a lower
    case before inserting.
    Privilege name returned by sentryProxy is always lower case,
    which might not match the privilegeName built in the catalog.
    This triggers an update of the catalog object followed by a
    removal of the old object. Since they both use the same key
    in lower case it ends up deleting the newly updated object.
    
    This change also adds a new catalogd startup option
    (sentry_catalog_polling_frequency)
    to configure the frequency at which catalogd polls the sentry service
    to update any policy changes. The default value is 60 seconds.
    
    Test:
    Added a test which adds select privileges to 3 tables and dbs specified
    in lower case, upper case and mixed case. The test verifies that the
    privileges on the 3 tables do not disappear on a sentry update.
    
    Change-Id: Ide3dfa601fcf77f5acc6adce9bea443aea600901

--- tests/authorization/test_grant_revoke.py
+++ tests/authorization/test_grant_revoke.py
@@ -26,6 +26,7 @@ from time import sleep
 from tests.common.custom_cluster_test_suite import CustomClusterTestSuite
 from tests.common.impala_test_suite import ImpalaTestSuite
 from tests.common.test_dimensions import create_uncompressed_text_dimension
+from tests.util.calculation_util import get_random_id
 from tests.verifiers.metric_verifier import MetricVerifier
 
 SENTRY_CONFIG_FILE = getenv('IMPALA_HOME') + '/fe/src/test/resources/sentry-site.xml'
@@ -86,6 +87,81 @@ class TestGrantRevoke(CustomClusterTestSuite, ImpalaTestSuite):
   def test_grant_revoke(self, vector):
     self.run_test_case('QueryTest/grant_revoke', vector, use_db="default")
 
+  @pytest.mark.execute_serially
+  @CustomClusterTestSuite.with_args(
+      impalad_args="--server_name=server1",
+      catalogd_args="--sentry_config=" + SENTRY_CONFIG_FILE +
+                    " --sentry_catalog_polling_frequency_s=1",
+      statestored_args="--statestore_update_frequency_ms=300")
+  def test_role_privilege_case(self, vector):
+    """IMPALA-5582: Store sentry privileges in lower case. This
+    test grants select privileges to roles assgined to tables/db
+    specified in lower, upper and mix cases. This test verifies
+    that these privileges do not vanish on a sentryProxy thread
+    update.
+    """
+    try:
+      db_name = "grant_rev_db_" + get_random_id(5)
+      db_name_upper_case = "XDB_" + get_random_id(5)
+      db_name_mixed_case = "YdB_" + get_random_id(5)
+      role_name = "test_role_" + get_random_id(5)
+      self.client.execute("create role {0}".
+          format(role_name))
+      self.client.execute("grant all on server to {0}".
+          format(role_name))
+      self.client.execute("grant role {0} to group {1}".
+          format(role_name, grp.getgrnam(getuser()).gr_name))
+
+      self.client.execute("create database " + db_name)
+      self.client.execute("create database " + db_name_upper_case)
+      self.client.execute("create database " + db_name_mixed_case)
+      self.client.execute("create table if not exists {0}.test1(i int)".
+          format(db_name))
+      self.client.execute("create table if not exists {0}.TEST2(i int)".
+          format(db_name))
+      self.client.execute("create table if not exists {0}.Test3(i int)".
+          format(db_name))
+
+      self.client.execute("grant select on table test1 to {0}".
+          format(role_name))
+      self.client.execute("grant select on table TEST2 to {0}".
+          format(role_name))
+      self.client.execute("grant select on table TesT3 to {0}".
+         format(role_name))
+      self.client.execute("grant all on database {0} to {1}".
+          format(db_name, role_name))
+      self.client.execute("grant all on database {0} to {1}".
+          format(db_name_upper_case, role_name))
+      self.client.execute("grant all on database {0} to {1}".
+          format(db_name_mixed_case, role_name))
+      result = self.client.execute("show grant role {0}".
+          format(role_name))
+      assert any('test1' in x for x in result.data)
+      assert any('test2' in x for x in result.data)
+      assert any('test3' in x for x in result.data)
+      assert any(db_name_upper_case.lower() in x for x in result.data)
+      assert any(db_name_mixed_case.lower() in x for x in result.data)
+      # Sleep for 2 seconds and make sure that the privileges
+      # on all 3 tables still persist on a sentryProxy thread
+      # update. sentry_catalog_polling_frequency_s is set to 1
+      # seconds.
+      sleep(3)
+      result = self.client.execute("show grant role {0}".
+          format(role_name))
+      assert any('test1' in x for x in result.data)
+      assert any('test2' in x for x in result.data)
+      assert any('test3' in x for x in result.data)
+      assert any(db_name_upper_case.lower() in x for x in result.data)
+      assert any(db_name_mixed_case.lower() in x for x in result.data)
+    finally:
+      self.client.execute("drop database if exists {0}".
+          format(db_name_upper_case))
+      self.client.execute("drop database if exists {0}".
+          format(db_name_mixed_case))
+      self.client.execute("drop database if exists {0} cascade".
+          format(db_name))
+      self.client.execute("drop role {0}".
+          format(role_name))
 
   @pytest.mark.execute_serially
   @CustomClusterTestSuite.with_args(
@@ -110,7 +186,7 @@ class TestGrantRevoke(CustomClusterTestSuite, ImpalaTestSuite):
     privileges_before = self.client.execute("show grant role test_role")
     # Wait a few seconds before restarting Impalad to make sure that the Catalog gets
     # updated.
-    sleep(3)
+    sleep(2)
     self.restart_first_impalad()
     verifier = MetricVerifier(self.cluster.impalads[0].service)
     verifier.wait_for_metric("catalog.ready", True)
