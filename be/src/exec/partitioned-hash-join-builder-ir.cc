750614100af1bf71394be1a9b5a745973829da49
aphadke <aphadke@cloudera.com>
2017-08-18 17:02:02 -0700

    IMPALA-3897 Codegen null-aware constant in PHJ::ProcessBuildBatch()
    This change codegen outs a branch in ProcessBuildBatch(). The branch never gets
    executed for most of the join types except NULL_AWARE_LEFT_ANTI_JOIN.
    The branch itself is not expensive to execute, but it will reduce codegen
    time by removing the dead code inside the branch for almost all join
    modes.
    
    Change-Id: I06acbebc9d2d23bef4734b480a5d3ce41680ea70

--- be/src/exec/partitioned-hash-join-builder-ir.cc
+++ be/src/exec/partitioned-hash-join-builder-ir.cc
@@ -37,14 +37,15 @@ inline bool PhjBuilder::AppendRow(
 }
 
 Status PhjBuilder::ProcessBuildBatch(
-    RowBatch* build_batch, HashTableCtx* ctx, bool build_filters) {
+    RowBatch* build_batch, HashTableCtx* ctx, bool build_filters, bool null_aware) {
   Status status;
   HashTableCtx::ExprValuesCache* expr_vals_cache = ctx->expr_values_cache();
   expr_vals_cache->Reset();
   FOREACH_ROW(build_batch, 0, build_batch_iter) {
     TupleRow* build_row = build_batch_iter.Get();
     if (!ctx->EvalAndHashBuild(build_row)) {
-      if (null_aware_partition_ != NULL) {
+      if (null_aware) {
+        DCHECK(true);
         // TODO: remove with codegen/template
         // If we are NULL aware and this build row has NULL in the eq join slot,
         // append it to the null_aware partition. We will need it later.
